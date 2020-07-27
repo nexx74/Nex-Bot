0f5d6e7487c898543cc53385dd9721bced709990
nexx74 <62615814+nexx74@users.noreply.github.com>
2020-07-27 13:08:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -376,33 +376,6 @@ message.channel.setRateLimitPerUser(duration)
 message.channel.send("I have set the slowmode in this channel to " + duration + " seconds!")
 }
 
-if (message.content.startsWith('m;verify')) {
-  if (message.author.bot){
-    return;
-  }
-  message.channel.send(message.author.toString() + "** Do you want to join our server? Say yes or no** ")
-      .then(() => {
-          message.channel.awaitMessages(response => response.content == 'yes' || response.content == 'no', {
-                  max: 1,
-                  time: 60000,
-                  errors: ['time'],
-              })
-
-              .then((collected) => {
-                  if (collected.first().content == 'yes') {
-                      message.channel.send(`*welcome to the server wait a few seconds to get full acess*`);
-                      let mainRole = message.guild.roles.cache.find(role => role.name === "Stars");
-                      message.member.roles.add(mainRole.id);
-                      console.log("done");
-                  } else if (collected.first().content == 'no') {
-                      message.channel.send(`*loser fine leave and find another server bitch*`);
-                  }
-              })
-              .catch(() => {
-                  message.channel.send(`redo this and say yes or no`);
-              });
-      });
-}
 
 })
 
